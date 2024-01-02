68cee3242a6b2bbd14bfd14413a468e542ef7baa
costincca <60868050+costincca@users.noreply.github.com>
2024-01-02 03:52:02 +0200

    Added 2 new vizualizers panes and the a uniform style button

--- web-styles-debug-window.js
+++ web-styles-debug-window.js
@@ -1,7 +1,7 @@
 /******************************************/
 /* Constantin Coșoiu - Bucharest, Romania */
 /******************************************/
-/* 2023-12-30 * ver. 1.1 * CC 0 Universal */
+/* 2024-01-01 * ver. 1.3 * CC 0 Universal */
 /******************************************/
 
 var stylesToShow = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'P', 'A'];
@@ -11,29 +11,28 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const debug = urlParams.get('dbg');
 const debugColumns = urlParams.get('dbgcol');
+const columnsDesired = parseInt(debugColumns);
+if(columnsDesired > 0) { columnsForStylesToShow = columnsDesired; }
+const templatedColumns = Array(columnsForStylesToShow + 1).join("1fr ").trim();
 
-if(debug > "")
-{
-	var columnsDesired = parseInt(debugColumns);
-	if(columnsDesired > 0)
-	{
-		columnsForStylesToShow = columnsDesired;
-	}
-	createDebugPanel();
+if(debug === "1") { //Full draggable debug panel
+	createFullDebugPanel();
 
-	// Make the DIV element draggable:
+	// Make the DIV element draggable
 	dragElement(document.getElementById("dbgWnd"));
+} else if(debug === "2") { // Simple semitransparent non-draggable debug panel floating on top
+	createFixedDebugPanel();
+} else if(debug === "3") { // Simple scrolling debug panel on top of the page
+	createStickyDebugPanel();
 }
 
-function createDebugPanel()
+function createFullDebugPanel()
 {
-	var templatedColumns = Array(columnsForStylesToShow + 1).join("1fr ").trim();
-	
 	var txt = `
 		<style>
 			#dbgWnd {
 			  position: absolute;
-			  z-index: 9;
+			  z-index: 10000;
 			  background-color: #f1f1f1;
 			  border: 1px solid #333333;
 			  text-align: center;
@@ -87,6 +86,13 @@ function createDebugPanel()
 			.dbgWndAddStyles {
 				background-color: purple;
 			}
+			
+			.dbgWndUniformStyle {
+				font-size: 20px;
+				color: black;
+				text-shadow: 1px 1px white;
+				font-weight: bold;
+			}
 		</style>
 		<!-- Draggable DIV -->
 		<div id="dbgWnd">
@@ -108,11 +114,267 @@ function createDebugPanel()
 		txt += `
 			</div>
 			<div id="dbgWndFooter">
-				<button class="dbgWndButton dbgWndRecalc" onclick="populateFixedDetails();populateVariableDetails();">Recalculate</button>
-				<button class="dbgWndButton dbgWndAddStyles" onclick="showInlineStyles();">+ Add Styles to Elements</button>
+				<button class="dbgWndButton dbgWndRecalc" onclick="deactivateUniformStyle();populateFixedDetails();populateVariableDetails();">Recalculate</button>
+				<button class="dbgWndButton dbgWndToggleStyles" onclick="toggleUniformStyle();">Toggle Element Style</button>
+				<button class="dbgWndButton dbgWndAddStyles" onclick="showInlineStyles();disableButton(this);">+ Add Styles to Elements</button>
 			</div>
 		</div>`;
 	document.body.insertAdjacentHTML('afterbegin', txt);
+}
+
+function createFixedDebugPanel()
+{
+	var txt = `
+		<style>
+			#dbgWnd {
+			  position: fixed;
+			  top: 0;
+			  left: 0;
+			  width: 100%;
+			  height: auto;
+			  z-index: 10000;
+			  background-color: rgba(128, 128, 128, 0.3);
+			  border: 1px solid #333333;
+			  text-align: center;
+			  text-shadow: 1px 1px white, 0px 0px 5px white, 0px 0px 10px white;
+			}
+
+			#dbgWndHeader {
+				cursor: move;
+			}
+
+			#dbgWndHeader, #dbgWndFooter {
+			  padding: 10px;
+			  z-index: 10;
+			  background-color: rgba(128, 128, 128, 0.3);
+			  color: #fff;
+			}
+			
+			#dbgWndFixedDetails {
+				display: grid;
+				grid-template-columns: 1fr 1fr 1fr;
+				grid-gap: 3px;
+				font-weight: bold;
+			}
+			
+			#dbgWndVariableDetails {
+				display: grid;
+				grid-template-columns: ${templatedColumns};
+				grid-gap: 3px;
+			}
+			
+			.dbgWndVariableDetailsStyle {
+				margin-left: 5px;
+				margin-right: 5px;
+				border: 1px solid grey;
+				align-self: center;
+				background-color: transparent;
+				border-radius: 3px;
+			}
+			
+			.dbgWndButton {
+				background-color: transparent;
+				color: white;
+				border: 1px solid white;
+				border-radius: 3px;
+				padding: 3px 5px;
+			}
+
+			.dbgWndButton:hover {
+				background-color: white;
+				color: #007bff;
+				border-radius: 3px;
+				cursor: pointer;
+			}
+			
+			.dbgWndRecalc {
+				background-color: rgba(0, 255, 0, 0.2);
+				color: black;
+				text-shadow: 1px 1px white;
+				font-weight: bold;
+			}
+			
+			.dbgWndToggleStyles {
+				background-color: rgba(0, 0, 255, 0.2);
+				color: black;
+				text-shadow: 1px 1px white;
+				font-weight: bold;
+			}
+			
+			.dbgWndAddStyles {
+				background-color: rgba(128,0,128, 0.2);
+				color: black;
+				text-shadow: 1px 1px white;
+				font-weight: bold;
+			}
+			
+			.dbgWndUniformStyle {
+				font-size: 20px;
+				color: black;
+				text-shadow: 1px 1px white;
+				font-weight: bold;
+			}
+		</style>
+		<!-- Draggable DIV -->
+		<div id="dbgWnd">
+			<div id="dbgWndFixedDetails">
+				<div>DPR: <span id="dbgWndDetailsDPR">-</span></div>
+				<div>Width: <span id="dbgWndDetailsInnerWidth">- </span>px</div>
+				<div>Height: <span id="dbgWndDetailsInnerHeight">- </span>px</div>
+			</div>`;
+		txt += `
+			<div id="dbgWndVariableDetails">`;
+				stylesToShow.forEach((elem) => {
+						txt += `<${elem} class="dbgWndVariableDetailsStyle">${elem}: <span id="dbgWndStyle${elem}">-</span></${elem}>`;
+					}
+				);
+		txt += `
+			</div>
+			<div id="dbgWndFooter">
+				<button class="dbgWndButton dbgWndRecalc" onclick="deactivateUniformStyle();populateFixedDetails();populateVariableDetails();">Recalculate</button>
+				<button class="dbgWndButton dbgWndToggleStyles" onclick="toggleUniformStyle();">Toggle Element Style</button>
+				<button class="dbgWndButton dbgWndAddStyles" onclick="showInlineStyles();disableButton(this);">+ Add Styles to Elements</button>
+			</div>
+		</div>`;
+	document.body.insertAdjacentHTML('afterbegin', txt);
+}
+
+function createStickyDebugPanel()
+{
+	var txt = `
+		<style>
+			#dbgWnd {
+			  position: sticky;
+			  top: 0;
+			  left: 0;
+			  width: 100%;
+			  height: auto;
+			  z-index: 10000;
+			  background-color: #f1f1f1;
+			  border: 1px solid #333333;
+			  text-align: center;
+			  text-shadow: 1px 1px white, 0px 0px 5px white, 0px 0px 10px white;
+			}
+
+			#dbgWndHeader {
+				cursor: move;
+			}
+
+			#dbgWndHeader, #dbgWndFooter {
+			  padding: 10px;
+			  z-index: 10;
+			  background-color: rgba(128, 128, 128, 0.3);
+			  color: #fff;
+			}
+			
+			#dbgWndFixedDetails {
+				display: grid;
+				grid-template-columns: 1fr 1fr 1fr;
+				grid-gap: 3px;
+				
+				background-color: #dddddd;
+				font-weight: bold;
+			}
+			
+			#dbgWndVariableDetails {
+				display: grid;
+				grid-template-columns: ${templatedColumns};
+				grid-gap: 3px;
+
+				background-color: #dddddd;
+			}
+			
+			.dbgWndVariableDetailsStyle {
+				margin-left: 5px;
+				margin-right: 5px;
+				border: 1px solid grey;
+				align-self: center;
+				background-color: white;
+				border-radius: 3px;
+			}
+			
+			.dbgWndButton {
+				background-color: transparent;
+				color: white;
+				border: 1px solid white;
+				border-radius: 3px;
+				padding: 3px 5px;
+			}
+
+			.dbgWndButton:hover {
+				background-color: white;
+				color: #007bff;
+				border-radius: 3px;
+				cursor: pointer;
+			}
+			
+			.dbgWndRecalc {
+				background-color: green;
+				color: white;
+				font-weight: bold;
+			}
+			
+			.dbgWndToggleStyles {
+				background-color: rgba(0, 0, 255, 0.6);
+				color: white;
+				font-weight: bold;
+			}
+			
+			.dbgWndAddStyles {
+				background-color: purple;
+				color: white;
+				font-weight: bold;
+			}
+			
+			.dbgWndUniformStyle {
+				font-size: 20px;
+				color: black;
+				text-shadow: 1px 1px white;
+				font-weight: bold;
+			}
+		</style>
+		<!-- Draggable DIV -->
+		<div id="dbgWnd">
+			<div id="dbgWndFixedDetails">
+				<div>DPR: <span id="dbgWndDetailsDPR">-</span></div>
+				<div>Width: <span id="dbgWndDetailsInnerWidth">- </span>px</div>
+				<div>Height: <span id="dbgWndDetailsInnerHeight">- </span>px</div>
+			</div>`;
+		txt += `
+			<div id="dbgWndVariableDetails">`;
+				stylesToShow.forEach((elem) => {
+						txt += `<${elem} class="dbgWndVariableDetailsStyle">${elem}: <span id="dbgWndStyle${elem}">-</span></${elem}>`;
+					}
+				);
+		txt += `
+			</div>
+			<div id="dbgWndFooter">
+				<button class="dbgWndButton dbgWndRecalc" onclick="deactivateUniformStyle();populateFixedDetails();populateVariableDetails();">Recalculate</button>
+				<button class="dbgWndButton dbgWndToggleStyles" onclick="toggleUniformStyle();">Toggle Element Style</button>
+				<button class="dbgWndButton dbgWndAddStyles" onclick="showInlineStyles();disableButton(this);">+ Add Styles to Elements</button>
+			</div>
+		</div>`;
+	document.body.insertAdjacentHTML('afterbegin', txt);
+}
+
+function disableButton(button) {
+	button.disabled = true;
+	button.style.backgroundColor = "lightgrey";
+	button.innerHTML = "Already added style";
+}
+
+function deactivateUniformStyle() {
+	var foundItems = document.getElementsByClassName("dbgWndVariableDetailsStyle");
+	for (let found of foundItems) {
+		found.classList.remove("dbgWndUniformStyle");
+	};
+}
+
+function toggleUniformStyle() {
+	var foundItems = document.getElementsByClassName("dbgWndVariableDetailsStyle");
+	for (let found of foundItems) {
+		found.classList.toggle("dbgWndUniformStyle");
+	};
 }
 
 function populateFixedDetails() {
@@ -126,7 +388,6 @@ function populateVariableDetails() {
 			let found =  document.getElementById(`dbgWndStyle${elem}`);
 			let elementStyle = getComputedStyle(found);
 			found.innerHTML = elementStyle.fontSize;
-			console.log(elementStyle);
 		}
 	);	
 }
